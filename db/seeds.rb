71b0a19b7e0d076feb9a1db186d246729c09b779
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2021-12-07 17:27:26 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+author_one = Author.find_or_create_by!(name: 'author_one')
+author_two = Author.find_or_create_by!(name: 'author_two')
+
+book_one = Book.find_or_create_by!(name: 'book_one', author_id: author_one.id, book_type: 'magazine')
+book_two = Book.find_or_create_by!(name: 'book_two', author_id: author_one.id, book_type: 'comic')
+book_three = Book.find_or_create_by!(name: 'book_three', author_id: author_two.id, book_type: 'magazine')
+book_four = Book.find_or_create_by!(name: 'book_four', author_id: author_two.id, book_type: 'comic')
